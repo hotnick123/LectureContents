bd2fbf9a081e0e6e19636b6743f31379bde06361
jdy8739 <jdy8739@naver.com>
2021-07-16 12:39:00 +0900

    0716 숙제 vue

---
+++ javascript/DoyoungJeong/frontend-down/src/views/MemberListPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="member">
+        <h3>Vue 회원가입 리스트</h3>
+        <router-link :to ="{ name: 'MemberRegisterPage' }">
+            <h4>회원가입하기</h4>
+        </router-link>
+        
+    </div>
+</template>
+
+<script>
+
+export default {
+    
+}
+</script>
