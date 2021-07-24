3ded733d8bea3b8b788c5c21e2f7b118a48089b7
jdy8739 <jdy8739@naver.com>
2021-07-24 14:13:18 +0900

    0726 숙제 vue, 위에것 잘못올림

---
+++ javascript/DoyoungJeong/frontend-down/src/views/student/StudentScoresPage.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h3>Student Scores Board</h3>
+        <student-scores :students="students"></student-scores>
+        <assessment :students="students"></assessment>
+    </div>
+</template>
+
+<script>
+import StudentScores from '@/components/studentScores/StudentScores.vue'
+import Assessment from '@/components/studentScores/Assessment.vue'
+import { mapActions, mapState } from 'vuex'
+
+export default {
+    name: 'StudentScoresPage',
+    components: {
+        StudentScores,
+        Assessment
+    },
+    computed: {
+        ...mapState(['students'])
+    },
+    mounted() {
+        this.fetchStudentList()
+    },
+    methods: {
+        ...mapActions(['fetchStudentList'])
+    }
+}
+</script>
