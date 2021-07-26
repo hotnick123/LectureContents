e54514d674f8e50b336ccf2c36b97ffc45d1b84c
jdy8739 <jdy8739@naver.com>
2021-07-27 01:15:06 +0900

    0727 prob89 스프링

---
+++ javascript/DoyoungJeong/project/src/main/java/com/example/demo/entity/VueTriangle.java
@@ -0,0 +1,13 @@
+package com.example.demo.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Setter
+@Getter
+public class VueTriangle {
+    private Integer widthSpring;
+    private Integer heightSpring;
+}
