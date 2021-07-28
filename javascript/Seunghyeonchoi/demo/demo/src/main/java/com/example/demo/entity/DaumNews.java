a4666243e704fa964d04e668870471f1aabd26bd
KH-CSH <endras132@naver.com>
2021-07-28 16:26:17 +0900

    58일차 내용 이동

---
+++ javascript/Seunghyeonchoi/demo/demo/src/main/java/com/example/demo/entity/DaumNews.java
@@ -0,0 +1,14 @@
+package com.example.demo.entity;
+
+
+import com.sun.javafx.beans.IDProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class DaumNews {
+    @Id
+
+}
