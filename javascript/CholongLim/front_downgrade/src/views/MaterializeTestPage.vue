97df9761b8be521aea2482e7c97efb7bf08dd582
limcholong <limcho1ong5@naver.com>
2021-07-30 15:15:24 +0900

    59일차 : Slot

---
+++ javascript/CholongLim/front_downgrade/src/views/MaterializeTestPage.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="home">
+        <div class="orange lighten-1 center-align">
+            Meterialize
+        </div>
+        <div class="white-text orange darken-5">
+            우와~
+        </div>
+    </div>
+</template>
---
+++ javascript/CholongLim/front_downgrade/src/views/NamedSlotTestPage.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="app">
+        <child-comp :ran_dog="randomDog">
+            <div slot="best">
+                <figure>
+                    <img src="@/assets/img/Malamute.jpg"/>
+                    <figcaption>Malamute</figcaption>
+                </figure>
+            </div>
+            <div slot="footer">
+                <footer>
+                    <p>&copy; All right reserved</p>
+                </footer>
+            </div>
+        </child-comp>
+    </div>
+</template>
+
+<script>
+
+import ChildComp from '@/components/slot/ChildComp'
+
+export default {
+    name: 'NamedSlotTestPage',
+    components: {
+        'child-comp': ChildComp
+    },
+    data () {
+        return {
+            dogs: [
+                { dog_name: 'Alaskan Malamute', pic:require('@/assets/img/Malamute.jpg')},
+                { dog_name: 'Dobermann Pinscher', pic:require('@/assets/img/Dobermann.jpg')},
+                { dog_name: 'German Shepherd', pic:require('@/assets/img/Shepherd.jpg')}
+            ]
+        }
+    },
+    computed: {
+        randomDog () {
+            return this.dogs[Math.floor(Math.random() * this.dogs.length)]
+        }
+    }
+}
+</script>
