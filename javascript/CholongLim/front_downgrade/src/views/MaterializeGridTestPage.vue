e0c5468ec3a27c5afda4280cfe247fd92e094517
limcholong <limcho1ong5@naver.com>
2021-08-01 18:39:28 +0900

    60일차 : UI/UX

---
+++ javascript/CholongLim/front_downgrade/src/views/MaterializeGridTestPage.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="container">
+        <!-- s는 휴대폰 설정
+                m은 태블릿 설정
+                l은 pc 설정
+                xl은 모니터 큰 pc 설정 -->
+        <div class="row flex">
+            <div class="col s2 m2 l2 xl2 red">
+                column 2
+            </div>
+            <div class="col s4 m4 l4 xl4 blue">
+                column 4
+            </div>
+            <div class="col s6 m6 l6 xl6 green">
+                column 6
+            </div>
+            <div class="col s12 m12 l12 xl12 teal">
+                column 12
+            </div>
+        </div>
+        <div class="row">
+            <a class="btn waves-effect waves-red blue" @click="showMessage()">
+                Wi-Fi ON!
+            </a>
+            <a class="btn-flat waves-effect waves-orange" @click="showMessage()">
+                <i class="material-icons left">cloud</i>
+                cloud
+            </a>
+            <a class="btn-flat waves-effect waves-yellow" @click="showMessage()">
+                <i class="material-icons left">dehaze</i>
+                MENU
+            </a>
+            <a class="btn-flat waves-effect waves-pink" @click="showMessage()">
+                <i class="material-icons center">account_circle</i>
+            </a>
+            <a class="btn-floating waves-effect waves-green" @click="showMessage()">
+                <i class="material-icons rifht">wifi</i>
+                wifi
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MaterializeGridTestPage',
+    methods: {
+        showMessage($event) {
+            alert($event)
+        }
+    }
+}
+</script>
