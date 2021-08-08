c2bac6d167e098d622fd60c8f76cef176368e830
hotnick123 <6547123@naver.com>
2021-08-09 02:07:32 +0900

    최혜주 0809 최신화

---
+++ javascript/HyejooChoi/jswithspring/src/main/java/com/example/jswithspring/repository/jpa/JPAMemberRepository.java
@@ -0,0 +1,14 @@
+package com.example.jswithspring.repository.jpa;
+
+import com.example.jswithspring.entity.jpa.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface JPAMemberRepository extends JpaRepository<Member, Long> {
+
+    @Query("select m from Member m join fetch m.authList where m.memberNo = :memberNo")
+    Optional<Member> findByAuth(Long memberNo);
+}
---
+++ javascript/HyejooChoi/jswithspring/src/main/java/com/example/jswithspring/service/jpa/JPAMemberServiceImpl.java
@@ -0,0 +1,48 @@
+package com.example.jswithspring.service.jpa;
+
+import com.example.jswithspring.controller.vue.jpa.request.MemberRequest;
+import com.example.jswithspring.entity.jpa.Member;
+import com.example.jswithspring.entity.jpa.MemberAuth;
+import com.example.jswithspring.repository.jpa.JPAMemberAuthRepository;
+import com.example.jswithspring.repository.jpa.JPAMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class JPAMemberServiceImpl implements JPAMemberService {
+
+    @Autowired
+    private JPAMemberRepository memberRepository;
+
+    @Autowired
+    private JPAMemberAuthRepository memberAuthRepository;
+
+    @Override
+    public void register(MemberRequest memberRequest) throws Exception {
+        MemberAuth authEntity = new MemberAuth(memberRequest.getAuth());
+        Member memberEntity = new Member(memberRequest.getUserId(), memberRequest.getPassword());
+        memberEntity.addAuth(authEntity);
+
+        memberRepository.save(memberEntity);
+    }
+
+    /*
+    @Override
+    public void login(Member member) throws Exception {
+        repository.login(member);
+    }
+
+    @Override
+    public List<Member> list() throws Exception {
+        return repository.list();
+    }
+     */
+
+    @Override
+    public Optional<Member> findByAuth(Long memberNo) {
+        return memberRepository.findByAuth(memberNo);
+    }
+}
