c1595d75b88623dd27c2cc5011a4e499454fafd5
hotnick123 <6547123@naver.com>
2021-10-01 19:35:06 +0900

    Update

--- javascript/HyejooChoi/jswithspring/src/main/java/com/example/jswithspring/service/jpa/JPAMemberServiceImpl.java
+++ javascript/HyejooChoi/jswithspring/src/main/java/com/example/jswithspring/service/jpa/JPAMemberServiceImpl.java
@@ -6,6 +6,7 @@ import com.example.jswithspring.entity.jpa.MemberAuth;
 import com.example.jswithspring.repository.jpa.JPAMemberAuthRepository;
 import com.example.jswithspring.repository.jpa.JPAMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,8 +21,14 @@ public class JPAMemberServiceImpl implements JPAMemberService {
     @Autowired
     private JPAMemberAuthRepository memberAuthRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public void register(MemberRequest memberRequest) throws Exception {
+        String encodeedPassword = passwordEncoder.encode(memberRequest.getPassword());
+        memberRequest.setPassword(encodeedPassword);
+
         MemberAuth authEntity = new MemberAuth(memberRequest.getAuth());
         Member memberEntity = new Member(memberRequest.getUserId(), memberRequest.getPassword());
         memberEntity.addAuth(authEntity);
@@ -29,12 +36,30 @@ public class JPAMemberServiceImpl implements JPAMemberService {
         memberRepository.save(memberEntity);
     }
 
-    /*
+
     @Override
     public void login(Member member) throws Exception {
-        repository.login(member);
+        Optional<Member> loginMember = memberRepository.findByUserId(member.getUserId());
+
+        if (loginMember == null)
+        {
+            log.info("login(): 그런 사람 없다.")
+            return false;
+        }
+
+        Member loginMember = maybeMember.get();
+
+        if (!passwordEncoder.matches(member.getPassword(), loginMember.getPassword()))
+        {
+            log.info("login(): 비밀번호 잘못 입력하였습니다.")
+            return false;
+        }
+
+        return false;
+
     }
 
+ /*
     @Override
     public List<Member> list() throws Exception {
         return repository.list();
